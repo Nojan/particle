af3eafe720a036823543765c53c25add3cded4b7
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2015-04-04 16:14:05 +0200

    VisualDebug: add sphere support

--- sources/visualdebug_renderer.hpp
+++ sources/visualdebug_renderer.hpp
@@ -29,6 +29,16 @@ private:
     float mSize;
 };
 
+class VisualDebugSphereCommand : public IVisualDebugCommand {
+public:
+    VisualDebugSphereCommand(const glm::vec3& position, const float radius);
+
+    void ApplyCommand(std::vector<glm::vec3>& vertex, std::vector<uint>& index) const override;
+private:
+    glm::vec3 mPosition;
+    float mRadius;
+};
+
 class VisualDebugRenderer {
 public:
     VisualDebugRenderer();
