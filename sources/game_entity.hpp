d575442de4ed9864ca9f299dc55184bfa0e57f40
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2015-07-18 21:43:33 +0200

    private GameEntity::addComponent GameEntity::removeComponent

--- sources/game_entity.hpp
+++ sources/game_entity.hpp
@@ -11,21 +11,6 @@ public:
     ~GameEntity();
 
     template <typename T>
-    void addComponent(T * componentPointer)
-    {
-        const std::type_index index = std::type_index(typeid(T));
-        void* pointer = reinterpret_cast<void*>(componentPointer);
-        addUntypedComponent(index, pointer);
-    };
-
-    template <typename T>
-    void removeComponent()
-    {
-        const std::type_index index = std::type_index(typeid(T));
-        removeUntypedComponent(index);
-    };
-
-    template <typename T>
     T* getComponent() 
     { 
         const std::type_index index = std::type_index(typeid(T));
@@ -33,6 +18,20 @@ public:
         return reinterpret_cast<T*>(pointer);
     };
 private:
+    friend class IComponentSystem;
+    template <typename T>
+    void addComponent(T * componentPointer)
+    {
+        const std::type_index index = std::type_index(typeid(T));
+        void* pointer = reinterpret_cast<void*>(componentPointer);
+        addUntypedComponent(index, pointer);
+    };
+    template <typename T>
+    void removeComponent()
+    {
+        const std::type_index index = std::type_index(typeid(T));
+        removeUntypedComponent(index);
+    };
     void addUntypedComponent(std::type_index index, void * untypedPointer);
     void removeUntypedComponent(std::type_index index);
     void* getUntypedComponent(std::type_index index);
