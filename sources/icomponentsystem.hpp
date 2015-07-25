6afc5e51ac1ac5d385bc256e156814be82ad9ac1
mathias.labeyrie@gmail.com <mathias.labeyrie@gmail.com>
2015-07-25 18:26:36 +0200

    Fix swap unmovable component

--- sources/icomponentsystem.hpp
+++ sources/icomponentsystem.hpp
@@ -7,6 +7,19 @@
 #include <memory>
 #include <vector>
 
+namespace Component {
+
+template <typename T>
+const T UnitializedValue();
+
+template <typename T>
+bool Initialized(const T& component)
+{
+    return UnitializedValue<T>() != component;
+}
+
+}
+
 class IComponentSystem : public IUpdater {
 public:
     virtual void attachEntity(GameEntity* entity) = 0;
@@ -51,8 +64,17 @@ protected:
     {
         assert(nullptr != entity);
         assert(nullptr == entity->getComponent<T>());
-        componentList.push_back(T());
-        T& component = componentList.back();
+        const size_t componentListSize = componentList.size();
+        size_t i;
+        for (i = 0; i < componentListSize; ++i)
+        {
+            if (!Component::Initialized(componentList[i]))
+                componentList[i] = T();
+        }
+        if (componentListSize == i)
+            componentList.push_back(T());
+
+        T& component = componentList[i];
         entity->addComponent<T>(&component);
         assert(&component == entity->getComponent<T>());
         return component;
@@ -70,9 +92,7 @@ protected:
             T& component = componentList[i];
             if (entityComponent == &component)
             {
-                const size_t newComponentCount = componentsCount - 1;
-                std::swap(componentList[i], componentList[newComponentCount]);
-                componentList.resize(newComponentCount);
+                componentList[i] = Component::UnitializedValue<T>();
                 break;
             }
         }
--- sources/transform_system.hpp
+++ sources/transform_system.hpp
@@ -5,6 +5,22 @@
 #include <glm/glm.hpp>
 #include <vector>
 
+namespace Component{
+
+template <>
+inline const glm::mat4 UnitializedValue()
+{
+    return glm::mat4(0);
+}
+
+template <>
+inline bool Initialized(const glm::mat4& component)
+{
+    return UnitializedValue<glm::mat4>() != component;
+}
+
+}
+
 class TransformSystem : public IComponentSystem {
 public:
     TransformSystem();
