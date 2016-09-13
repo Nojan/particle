1281f4e0f435cdb730e9a2d138a577f868f3180c
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2016-09-12 22:36:25 -0400

    Add numeric_cast

--- sources/types.hpp
+++ sources/types.hpp
@@ -3,7 +3,19 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <cassert>
+#include <type_traits>
 
 typedef unsigned int uint;
+
+template<typename To, typename From>
+To numeric_cast(From value)
+{
+    static_assert( std::is_arithmetic<From>::value, "");
+    static_assert( std::is_arithmetic<To>::value, "");
+    const To ToValue = static_cast<To>(value);
+    assert(static_cast<From>(ToValue) == value);
+    return ToValue;
+}
 
 #endif
