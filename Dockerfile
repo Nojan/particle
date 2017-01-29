448c2c251d630528deffab6e69c94059ff706ec6
Mathias Labeyrie <mathias.Labeyrie@gmail.com>
2017-01-29 14:02:43 -0500

    Switch build system to Docker.
    Travis CI is quite hard to work, and the build environment is outdated

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu:16.04
+
+WORKDIR /workdir
+RUN apt -y update
+RUN apt -y install gcc premake4 libsdl2-dev
+
+CMD ./build.sh
--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-$CXX --version
+
 eval "rm -Rf build"
 eval "premake4 gmake"
 cd build
-eval make CC='gcc-4.9' CXX='g++-4.9'
+eval make
 ret_code=$?
 cd ..
 exit $ret_code
