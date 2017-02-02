67d3d994391f3eaa123d53a87b971121d55ae4ec
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2017-02-01 21:38:06 -0500

    Audio use RingBuffer

--- sources/sound_system.cpp
+++ sources/sound_system.cpp
@@ -1,6 +1,7 @@
 #include "sound_system.hpp"
 
 #include "game_entity.hpp"
+#include "types.hpp"
 
 #include <SDL2/SDL.h>
 #include <cassert>
@@ -19,16 +20,118 @@ void SoundComponent::Play(const float deltaTime)
 
 }
 
+template <typename T, int16_t N>
+class RingBuffer {
+public:
+    RingBuffer()
+    : mSize(0)
+    , mWriteIndex(0)
+    {}
+
+    void write(int16_t& count, const T* data)
+    {
+        count = std::min<int16_t>(count, max_size() - size());
+        int16_t max_seq_write = std::min<int16_t>(count, max_size() - mWriteIndex);
+        if (count <= max_seq_write)
+        {
+            memcpy(&(mBuffer[mWriteIndex]), data, count*sizeof(T));
+            mWriteIndex = (mWriteIndex + count) % max_size();
+        }
+        else
+        {
+            memcpy(&(mBuffer[mWriteIndex]), data, max_seq_write * sizeof(T));
+            mWriteIndex = count - max_seq_write;
+            memcpy(&(mBuffer[0]), data + max_seq_write, mWriteIndex * sizeof(T));
+        }
+        mSize += count;
+        assert(mSize <= max_size());
+    }
+
+    void write(const T& element)
+    {
+        assert(mSize+1 <= max_size());
+        mBuffer[mWriteIndex] = element;
+        ++mWriteIndex;
+        if (max_size() <= mWriteIndex)
+            mWriteIndex -= max_size();
+        ++mSize;
+    }
+
+    void read(int16_t* count, T* data)
+    {
+        int16_t clamped_count = std::min<int16_t>(*count, size());
+        int16_t readIndex = read_index();
+        assert(0 <= readIndex);
+        assert(readIndex < max_size());
+        int16_t max_seq_read = std::min<int16_t>(clamped_count, max_size() - readIndex);
+        if (clamped_count <= max_seq_read)
+        {
+            memcpy(data, &(mBuffer[readIndex]), clamped_count * sizeof(T));
+        }
+        else
+        {
+            memcpy(data, &(mBuffer[readIndex]), max_seq_read * sizeof(T));
+            memcpy(data + max_seq_read, &(mBuffer[0]), (clamped_count - max_seq_read) * sizeof(T));
+        }
+        mSize -= clamped_count;
+        assert(0 <= mSize);
+        *count = clamped_count;
+    }
+
+    const T* read_ptr(int16_t* count)
+    {
+        const T* result = nullptr;
+        int16_t clamped_count = std::min<int16_t>(*count, size());
+        if (0 < clamped_count)
+        {
+            int16_t readIndex = read_index();
+            assert(0 <= readIndex);
+            assert(readIndex < max_size());
+            result = &(mBuffer[readIndex]);
+            clamped_count = std::min<int16_t>(clamped_count, max_size() - readIndex);
+            mSize -= clamped_count;
+            assert(0 <= mSize); 
+        }
+        *count = clamped_count;
+        return result;
+    }
+
+    int16_t read_index() const
+    {
+        int16_t index = (mWriteIndex - size());
+        if (index < 0)
+            return max_size() + index;
+        else
+            return index;
+    }
+
+    int16_t max_size() const
+    {
+        return numeric_cast<int16_t>(mBuffer.max_size());
+    }
+
+    int16_t size() const
+    {
+        return mSize;
+    }
+
+private:
+    std::array<T, N> mBuffer;
+    int16_t mSize;
+    int16_t mWriteIndex;
+};
+
 class SoundSystemImpl {
 public:
     SoundSystemImpl();
+
+    void fillBuffer(int period);
 
     SDL_AudioSpec mAudioSpecRequest;
     SDL_AudioSpec mAudioSpecObtained;
     SDL_AudioDeviceID mAudioDeviceId;
 
-    std::array<float, 4096> mBuffer;
-    size_t mBufferIndex;
+    RingBuffer<float, 4096> mBuffer;
 };
 
 SoundSystemImpl::SoundSystemImpl()
@@ -49,16 +152,22 @@ SoundSystemImpl::SoundSystemImpl()
         exit(EXIT_FAILURE);
     }
 
-    const float two_pi = 6.283185307179586476925f;
-    const int sample = 128;
-    const float sampleCountInv = 1.f / static_cast<float>(sample);
-    for (size_t idx = 0; idx < mBuffer.max_size(); ++idx)
-    {
-        const float cursor = static_cast<float>(idx) * sampleCountInv;
-        mBuffer[idx] = sin(cursor*two_pi);
-    }
+    fillBuffer(128);
+}
 
-    mBufferIndex = 0;
+void SoundSystemImpl::fillBuffer(int period)
+{
+    const float two_pi = 6.283185307179586476925f;
+    const float sampleCountInv = 1.f / static_cast<float>(period);
+    while (period <= (mBuffer.max_size() - mBuffer.size()))
+    {
+        for (int idx = 0; idx < period; ++idx)
+        {
+            const float cursor = static_cast<float>(idx) * sampleCountInv;
+            const float value = sin(cursor*two_pi);
+            mBuffer.write(value);
+        }
+    }
 }
 
 SoundSystem::SoundSystem()
@@ -80,34 +189,31 @@ void SoundSystem::Update(const float deltaTime)
     }
 
     const int freq = mImpl->mAudioSpecObtained.freq;
+    const int max_samples = mImpl->mAudioSpecObtained.samples;
     const uint32_t bytePerSample = sizeof(float);
-    const uint32_t audioDataMaxLen = mImpl->mBuffer.max_size();
-    uint32_t queue_index = mImpl->mBufferIndex;
     SDL_AudioDeviceID audioDeviceID = mImpl->mAudioDeviceId;
     uint32_t alreadyQueuedByte = SDL_GetQueuedAudioSize(audioDeviceID);
-    uint32_t alreadyQueued = alreadyQueuedByte / bytePerSample;
-    uint32_t toQueue = audioDataMaxLen - alreadyQueued;
-    uint32_t queue_index_new = std::min(queue_index + toQueue, audioDataMaxLen);
-    uint32_t effective_queue = queue_index_new - queue_index;
-    int audioError = SDL_QueueAudio(audioDeviceID, (const void*)&(mImpl->mBuffer[queue_index]), effective_queue * bytePerSample);
+    int16_t alreadyQueued = numeric_cast<int16_t>(alreadyQueuedByte / bytePerSample);
+    int16_t toQueue = max_samples - alreadyQueued;
+    int16_t firstReadCount = toQueue;
+    const float* firstReadSeq = mImpl->mBuffer.read_ptr(&firstReadCount);
+    int audioError = SDL_QueueAudio(audioDeviceID, (const void*)firstReadSeq, firstReadCount * bytePerSample);
     if (0 != audioError)
     {
         printf("Audio queue error %s\n", SDL_GetError());
     }
-    effective_queue = toQueue - effective_queue;
-    if (0 < effective_queue)
+    int16_t secondReadCount = toQueue - firstReadCount;
+    if (0 < secondReadCount)
     {
-        queue_index = 0;
-        queue_index_new = effective_queue;
-        int audioError = SDL_QueueAudio(audioDeviceID, (const void*)&(mImpl->mBuffer[queue_index]), effective_queue * bytePerSample);
+        const float* secondReadSeq = mImpl->mBuffer.read_ptr(&secondReadCount);
+        int audioError = SDL_QueueAudio(audioDeviceID, (const void*)secondReadSeq, secondReadCount * bytePerSample);
         if (0 != audioError)
         {
             printf("Audio queue error %s\n", SDL_GetError());
         }
     }
-    queue_index = queue_index_new % audioDataMaxLen;
-    mImpl->mBufferIndex = queue_index;
     SDL_PauseAudioDevice(audioDeviceID, 0);
+    mImpl->fillBuffer(128);
 }
 
 void SoundSystem::attachEntity(GameEntity* entity)
